fb3aae62b2848147dabeca6b56ca6db29d6b7cb7
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-01-09 18:25:53 +0530

    basic sidebar structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import MultiStepForm from "./components/MultiStepForm";
 
 function App() {
   return (
-    <div className="h-100 d-flex align-items-center justify-content-center">
+    <div className="h-100 d-flex align-items-center justify-content-center bg-light-grey">
       <MultiStepForm />
     </div>
   );
